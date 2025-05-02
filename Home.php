832d819f362092be94fc93e93df98c898d12551a
Black Air <blackairdigitalmarketing009@gmail.com>
2025-05-02 09:02:07 -0700

    updated h1 Home.php for live testing

--- Home.php
+++ Home.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main>
-    <h1>Latest Posts</h1>
+    <h1>WELCOME TO BLACK AIR EV FIRM</h1>
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post(); ?>
