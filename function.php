943937819159382df18cea71e07e2e43da371bdd
Black Air <blackairdigitalmarketing009@gmail.com>
2025-05-02 08:31:50 -0700

    Initialized functions.php and added theme support features

--- function.php
+++ function.php
@@ -1,14 +1,24 @@
 <?php
-// Theme setup
-function blackairelectric_theme_setup() {
-    add_theme_support('title-tag'); // Dynamic title
-    add_theme_support('post-thumbnails'); // Featured images
+// Add theme support
+function blackair_theme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
 }
-add_action('after_setup_theme', 'blackbulb_theme_setup');
+add_action('after_setup_theme', 'blackair_theme_setup');
 
-// Load Styles
-function blackairelectric_enqueue_styles() {
+// Register menus
+function blackair_register_menus() {
+    register_nav_menus(array(
+        'main-menu' => __('Main Menu', 'black-air-electric'),
+        'footer-menu' => __('Footer Menu', 'black-air-electric')
+    ));
+}
+add_action('init', 'blackair_register_menus');
+
+// Enqueue CSS and JS
+function blackair_enqueue_scripts() {
     wp_enqueue_style('style', get_stylesheet_uri());
 }
-add_action('wp_enqueue_scripts', 'blackairelectric_enqueue_styles');
+add_action('wp_enqueue_scripts', 'blackair_enqueue_scripts');
 ?>
