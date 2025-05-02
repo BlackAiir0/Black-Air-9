6fc0fe5f8d7348b32799967e5ea20f6f5e77cce8
Black Air <blackairdigitalmarketing009@gmail.com>
2025-05-03 01:25:58 +0530

    added home.php templete for home page custmization

--- Home.php
+++ Home.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main>
-    <h1>WELCOME TO BLACK AIR SOLAR FIRM</h1>
+    <h1>WELCOME TO BLACK AIR robot FIRM</h1>
     <?php
     if (have_posts()) :
         while (have_posts()) : the_post(); ?>
