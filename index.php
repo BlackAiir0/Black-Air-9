751123fbc4bf9829ceb4f299674745fb81ce866b
Black Air <blackairdigitalmarketing009@gmail.com>
2025-05-02 07:09:27 -0700

    updated index.php for deployment

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // fallback in case no template is found
 get_header();
-echo "welome In Black Air and black bulb for EEV -updated"
+echo "welome In Black Air and black ev for EEV -updated"
 get_footer();
 ?>
