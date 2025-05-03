f7e65dc3582dedde590359751ad9056255d0fec3
Black Air <blackairdigitalmarketing009@gmail.com>
2025-05-03 22:48:40 +0530

    updated single.php layout andadded style for single post

--- single.php
+++ single.php
@@ -22,3 +22,30 @@
 </div>
 
 <?php get_footer(); ?>
+<?php get_header(); ?>
+
+<div class="content-wrapper">
+    <main class="main-content">
+        <?php
+        if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+                <article <?php post_class(); ?>>
+                    <h1><?php the_title(); ?></h1>
+                    <?php if (has_post_thumbnail()) : ?>
+                        <div class="post-thumbnail">
+                            <?php the_post_thumbnail('large'); ?>
+                        </div>
+                    <?php endif; ?>
+                    <div class="post-content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+            <?php endwhile;
+        endif;
+        ?>
+    </main>
+
+    <?php get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
