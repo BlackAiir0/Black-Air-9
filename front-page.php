676b8738c4aeb3e7aea61f97358dad47e6caddf2
Black Air <blackairdigitalmarketing009@gmail.com>
2025-05-03 23:36:52 +0530

    styled blog post layout and updated front-page

--- front-page.php
+++ front-page.php
@@ -85,3 +85,23 @@
         <p>No posts found.</p>
     <?php endif; ?>
 </section>
+<?php get_header(); ?>
+
+<div class="blog-posts">
+  <?php
+  if ( have_posts() ) :
+    while ( have_posts() ) : the_post(); ?>
+      <div class="post">
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <div class="excerpt">
+          <?php the_excerpt(); ?>
+        </div>
+      </div>
+    <?php endwhile;
+  else :
+    echo '<p>No posts found.</p>';
+  endif;
+  ?>
+</div>
+
+<?php get_footer(); ?>
